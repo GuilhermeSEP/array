5dfc591a3ac1090b697c2cc39ad2795d947b3705
GuilhermeSEP <guilherme.gomessouza@hotmail.com>
2022-09-28 20:03:59 -0300

    array

---
+++ strings.php
@@ -0,0 +1,13 @@
+<?php
+
+$texto = "micro Computador";
+$tamanho = Strlen($texto); //tamanho da string
+echo "<br/>".$texto." possui ".$tamanho. " cacacteres";
+echo "<br/>".strtoupper($texto);
+echo "<br/>".strtolower($texto);
+echo "<br/>".ucfirst($texto);
+echo "<br/>".ucwords($texto);
+//3 caracteres a partir da posição 13
+echo "<br/>".substr($texto,13,3,);
+echo "<br/>".str_replace("micro", "Nano", $texto);
+?>
